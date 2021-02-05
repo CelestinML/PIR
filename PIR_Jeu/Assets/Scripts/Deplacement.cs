89cbf8b1c0c2e44f1152bab968553c0d1a89bfaf
ÃCelestinML <celestin.moenne@gmail.com>
2021-02-05 17:44:30 +0100

    Rework organisation + Script déplacement + background + bords avec collisions OK

---
+++ PIR_Jeu/Assets/Scripts/Deplacement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour
+{
+    private CharacterController controller;
+    private Transform transform;
+    private float input;
+
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = gameObject.transform;
+        input = 0;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        input = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(1, 0, 0) * Time.fixedDeltaTime * speed * input;
+    }
+}

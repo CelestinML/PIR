cd17b59a5f4bbc8ccd9ca952f32ef1798ba46436
ÃCelestinML <celestin.moenne@gmail.com>
2021-02-10 11:32:57 +0100

    Ajout projectile MAIS manque le gros cooldown (et lag un peu je crois)

---
+++ PIR_Jeu/Assets/Scripts/ProjectileBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileBehaviour : MonoBehaviour
+{
+
+    public float speed = 20;
+
+    private Transform transform;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = gameObject.transform;
+        cam = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(0, speed * Time.fixedDeltaTime, 0);
+        Vector3 position_in_camera = cam.WorldToViewportPoint(transform.position);
+        if (position_in_camera.y > 1.1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Asteroid")
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}

2869d5f2fe6456700b4935c50702a448b75022b1
evamnt <eva.minot@hotmail.fr>
2021-02-12 11:32:34 +0100

    ajout mainMenu

---
+++ PIR_Jeu/Assets/mainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class mainMenu : MonoBehaviour
+{
+    public void startGamePlayer()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void startGameIA()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
